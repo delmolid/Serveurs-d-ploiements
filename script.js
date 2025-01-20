e596e8218b17363cdc72e7dd9e1fa0dd909b12e7
Molid <delmolid@hotmail.fr>
2025-01-20 10:03:09 +0100

    suppression fichier popup.html content.js et manifest.json

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-document.getElementById("changeColor").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "changeColor" }, (response) => {
-            if (response?.status === "success") {
-                console.log("Couleur du titre changée !");
-            } else {
-                console.error("Erreur : Aucun élément H1 trouvé.");
-            }
-        });
-    });
-});
+
 
 
 
